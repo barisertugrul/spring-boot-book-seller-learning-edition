ce914b512cb8d3e95912d23b742b6374280d3554
Barış Ertuğrul <b.ertugrul.eposta@gmail.com>
2021-12-01 08:41:04 +0300

    Implementation of Internal Api Layer

--- src/main/java/com/ertyazilim/springbootbooksellertrainingedition/security/UserPrincipal.java
+++ src/main/java/com/ertyazilim/springbootbooksellertrainingedition/security/UserPrincipal.java
@@ -1,6 +1,8 @@
 package com.ertyazilim.springbootbooksellertrainingedition.security;
 
+import com.ertyazilim.springbootbooksellertrainingedition.model.Role;
 import com.ertyazilim.springbootbooksellertrainingedition.model.User;
+import com.ertyazilim.springbootbooksellertrainingedition.util.SecurityUtils;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +24,17 @@ public class UserPrincipal implements UserDetails {
     transient private String password;
     transient private User user;
     private Set<GrantedAuthority> authorities;
+
+    public static UserPrincipal createSuperUser()
+    {
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(Role.SYSTEM_MANAGER.name()));
+
+        return UserPrincipal.builder()
+                .id(-1L)
+                .username("system-administrator")
+                .authorities(authorities)
+                .build();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
